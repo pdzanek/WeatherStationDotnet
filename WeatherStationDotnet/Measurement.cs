070c926b839983e9803ea37606bde072d98e34e3
Piotr Janek <209332@edu.p.lodz.pl>
2019-04-23 16:37:39 +0200

    Attributes

--- WeatherStationDotnet/Measurement.cs
+++ WeatherStationDotnet/Measurement.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WeatherStationDotnet
 {
     class Measurement
     {
-        string key;
-        double value;
         Measurement(string key, double value)
         {
-            this.key = key;
-            this.value = value;
+            Key = key;
+            Value = value;
         }
+        string Key { get; set; }
+        double Value { get; set; }
     }
 }
